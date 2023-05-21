fe4eaabcf4285f3b24cca3914c191b78c95606ab
Gourgen KHACHATRIAN <gugo2828@gmail.com>
2023-05-21 20:08:32 +0200

    Project 68,frontend home page crud/css initial ok,put,update and read tested ok

--- src/component/ModalMenu.js
+++ src/component/ModalMenu.js
@@ -49,7 +49,7 @@ export default function MyComponent() {
   const handleUpdate = async (id) => {
     const projectsService = new ProjectsService();
     // Find the item being edited
-    const itemToUpdate = projects.find((item) => item.id === id);
+    /* const itemToUpdate = projects.find((item) => item.id === id); */
     // Update data
     const result = await projectsService.updateData(id, newData);
     setProjects((prevData) =>
@@ -75,150 +75,226 @@ export default function MyComponent() {
     setShowCreateForm((prevValue) => !prevValue);
   };
 
-  return (
+  return (<>
     <div>
- {/* Create form */}
- <h1>Create Project</h1>
-      {showCreateForm ? (
-        <form>
-          {/* Add input fields for each required field */}
-          <label>Name:</label>
-          <input
-            type="text"
-            value={newData.name}
-            onChange={(e) => setNewData({ ...newData, name: e.target.value })}
-          />
-          <label>Progress Rate:</label>
-          <input
-            type="text"
-            value={newData.progress_rate}
-            onChange={(e) => setNewData({ ...newData, progress_rate: e.target.value })}
-          />
-          <label>Status:</label>
-          <input
-            type="text"
-            value={newData.status}
-            onChange={(e) => setNewData({ ...newData, status: e.target.value })}
-          />
-            <label>Deadline:</label>
-          <input
-            type="text"
-            value={newData.dead_line}
-            onChange={(e) => setNewData({ ...newData, dead_line: e.target.value })}
-          />
-          <label>URL Remote Repository</label>
-          <input
-            type="text"
-            value={newData.url_remote_repository}
-            onChange={(e) => setNewData({ ...newData, url_remote_repository: e.target.value })}
-          />
-          
+ 
+      {/* Create form */}
+      <div className='div_home_add_new_project'>
+          {showCreateForm ? (
+           
+            <form>
+              {/* Add input fields for each required field */}
+              
+              <input
+                className="create-input"
+                type="text"
+                value={newData.name}
+                onChange={(e) => setNewData({ ...newData, name: e.target.value })}
+              placeholder='Name'/>
+              
+              <input
+                className="create-input"
+                type="text"
+                value={newData.progress_rate}
+                onChange={(e) => setNewData({ ...newData, progress_rate: e.target.value })}
+                placeholder='Progress Rate'/>
+              
+              <input
+                className="create-input"
+                type="text"
+                value={newData.status}
+                onChange={(e) => setNewData({ ...newData, status: e.target.value })}
+                placeholder='Status'/>
+                
+              <input
+                className="create-input"
+                type="text"
+                value={newData.dead_line}
+                onChange={(e) => setNewData({ ...newData, dead_line: e.target.value })}
+                placeholder='Deadline'/>
+              
+              <input
+                className="create-input"
+                type="text"
+                value={newData.url_remote_repository}
+                onChange={(e) => setNewData({ ...newData, url_remote_repository: e.target.value })}
+                placeholder='URL Remote Repository'/>
+              
 
-          <button type="button" onClick={handleCreate}>
-            Create
-          </button>
-        </form>
-      ) : (
-        <button onClick={toggleCreateForm}>Add</button>
-      )}
-      {/* table with projects */}
-      <table className='hometable'>
-        <thead>
+              <input
+                type="text"
+                value={newData.first_saving_date_time}
+                onChange={(e) => setNewData({ ...newData, first_saving_date_time: e.target.value })}
+                placeholder='First Saving Date/Time'/>
+
+              
+              <input
+                className="create-input"
+                type="text"
+                value={newData.last_update_date_time}
+                onChange={(e) => setNewData({ ...newData, last_update_date_time: e.target.value })}
+                placeholder='Last Update Date/Time'/>
+
+              <input
+                className="create-input"
+                type="text"
+                value={newData.version}
+                onChange={(e) => setNewData({ ...newData, version: e.target.value })}
+                placeholder='Version'/>
+
+              <button type="button" onClick={handleCreate}>
+                Create
+              </button>
+
+            </form>
+           
+            
+          ) : (
+            <button onClick={toggleCreateForm}>Add new project</button>
+          )}
+      </div>
+      <div className='div_hometable'>
+          {/* table with projects */}
+          <table className='hometable'>
+        
+          <thead>
           <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Progress Rate</th>
-            <th>Status</th>
-            <th>Deadline</th>
-            <th>URL Remote Repository</th>
-            <th>First Saving Date/Time</th>
-            <th>Last Update Date/Time</th>
-            <th>Version</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.name}
-                  onChange={(e) => setNewData({ ...newData, name: e.target.value })}
-                />
-              ) : item.name}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.progress_rate}
-                  onChange={(e) => setNewData({ ...newData, progress_rate: e.target.value })}
-                />
-              ) : item.progress_rate}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.status}
-                  onChange={(e) => setNewData({ ...newData, status: e.target.value })}
-                />
-              ) : item.status}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.dead_line}
-                  onChange={(e) => setNewData({ ...newData, dead_line: e.target.value })}
-                />
-              ) : item.dead_line}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.url_remote_repository}
-                  onChange={(e) =>
-                    setNewData({ ...newData, url_remote_repository: e.target.value })
-                  }
-                />
-              ) : item.url_remote_repository}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.first_saving_date_time}
-                  onChange={(e) =>
-                    setNewData({ ...newData, first_saving_date_time: e.target.value })
-                  }
-                />
-              ) : item.first_saving_date_time}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.last_update_date_time}
-                  onChange={(e) =>
-                    setNewData({ ...newData, last_update_date_time: e.target.value })
-                  }
-                />
-              ) : item.last_update_date_time}</td>
-              <td>{editingId === item.id ? (
-                <input
-                  type="text"
-                  value={newData.version}
-                  onChange={(e) => setNewData({ ...newData, version: e.target.value })}
-                />
-              ) : item.version}</td>
-              <td>
-                {editingId === item.id ? (
-                  <button onClick={() => handleUpdate(item.id)}>Save</button>
-                ) : (
-                  <>
-                    <button onClick={() => handleEdit(item.id)}>Edit</button>
-                    <button onClick={() => handleDelete(item.id)}>Delete</button>
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Progress Rate</th>
+                <th>Status</th>
+                <th>Deadline</th>
+                <th>URL Remote Repository</th>
+                <th>First Saving Date/Time</th>
+                <th>Last Update Date/Time</th>
+                <th>Version</th>
+                <th>Actions</th>
+              </tr>
+          </thead>
+          <tbody>
+            {projects.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
 
-     
-    </div>
-  );
+
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.name}
+                      onChange={(e) => setNewData({ ...newData, name: e.target.value })}
+                    />
+                  ) : (
+                    item.name
+                  )}
+                </td>
+              
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.progress_rate}
+                      onChange={(e) =>
+                        setNewData({ ...newData, progress_rate: e.target.value })
+                      }
+                    />
+                  ) : (
+                    item.progress_rate
+                  )}
+                </td>
+                {/* ... */}
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.status}
+                      onChange={(e) => setNewData({ ...newData, status: e.target.value })}
+                    />
+                  ) : (
+                    item.status
+                  )}
+                </td>
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.dead_line}
+                      onChange={(e) => setNewData({ ...newData, dead_line: e.target.value })}
+                    />
+                  ) : (
+                    item.dead_line
+                  )}
+                </td>
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.url_remote_repository}
+                      onChange={(e) => setNewData({ ...newData, url_remote_repository: e.target.value })}
+                    />
+                  ) : (
+                    item.url_remote_repository
+                  )}
+                </td>
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.first_saving_date_time}
+                      onChange={(e) => setNewData({ ...newData, first_saving_date_time: e.target.value })}
+                    />
+                  ) : (
+                    item.first_saving_date_time
+                  )}
+                </td>
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.last_update_date_time}
+                      onChange={(e) => setNewData({ ...newData, last_update_date_time: e.target.value })}
+                    />
+                  ) : (
+                    item.last_update_date_time
+                  )}
+                </td>
+                <td>
+                  {editingId === item.id ? (
+                    <input
+                      className="edit-input"
+                      type="text"
+                      value={newData.version}
+                      onChange={(e) => setNewData({ ...newData, version: e.target.value })}
+                    />
+                  ) : (
+                    item.version
+                  )}
+                </td>
+                
+              
+                <td>
+                  {editingId === item.id ? (
+                    <button onClick={() => handleUpdate(item.id)}>Save</button>
+                  ) : (
+                    <>
+                      <button onClick={() => handleEdit(item.id)}>Edit</button>
+                      <button onClick={() => handleDelete(item.id)}>Delete</button>
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        
+      </div>
+  </div>    
+  </>);
 }
